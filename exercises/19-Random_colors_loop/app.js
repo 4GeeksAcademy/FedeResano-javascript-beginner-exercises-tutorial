390f88023ddf23406f6c763bf4b4ff58f57c2b92
Federico Resano <123256603+fede1525@users.noreply.github.com>
2023-11-28 01:44:51 +0000

    commit

--- exercises/19-Random_colors_loop/app.js
+++ exercises/19-Random_colors_loop/app.js
@@ -16,9 +16,12 @@ function getColor(colorNumber = 0) {
 }
 
 function getAllStudentColors() {
-
+	for(let i = 0; i < 10; i++){
+		let randomNumber = Math.floor((Math.random() * 4) + 1);
+		console.log(getColor(randomNumber));
+	}
 	//your loop here
-	let exampleColor = getColor(1);
+	
 }
 
 //call the function below with the number of students in the class and print on the console
